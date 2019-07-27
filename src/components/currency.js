336c777db7753faaa8e95c6cd0cbcf053721bda4
Jeremey Lee <leejeremey@gmail.com>
2019-07-26 20:49:45 -0700

    t css changes

--- src/components/currency.js
+++ src/components/currency.js
@@ -10,10 +10,10 @@ const Currency = (props) => {
   return (
     <Row type="flex" justify="center">
       <Col>
-      <Row type="flex" justify="center">{props.currency}</Row>
-      <Row type="flex" justify="center">
-        <Button onClick={handleClick}>Increment</Button>
-      </Row>
+        <Row type="flex" justify="center">{props.currency}</Row>
+        <Row type="flex" justify="center">
+          <Button onClick={handleClick}>Increment</Button>
+        </Row>
       </Col>
     </Row>
   );
