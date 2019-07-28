53a455ba294ebb84499e1b4b4c1106930543ae3c
Jeremey Lee <leejeremey@gmail.com>
2019-07-27 18:15:03 -0700

    Cleaned up home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,38 +1,10 @@
 import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
 import Currency from './currency';
-import { incrementGold } from '../reducers/goldReducer';
 
 const Home = (props) => {
-  const currencyInterval = () => {
-    setTimeout(() => {
-      console.log('interval: ', props.interval);
-      props.incrementGold();
-      currencyInterval();
-    }, props.interval);
-  };
-
-  useEffect(() => {
-    console.log('loaded');
-    currencyInterval();
-  },[]);
-
   return (
     <Currency />
   );
 }
 
-const mapStateToProps = state => (
-  {
-    navigation: state.navigation,
-    interval: state.interval,
-  }
-);
-
-const mapDispatchToProps = (
-  {
-    incrementGold,
-  }
-)
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
